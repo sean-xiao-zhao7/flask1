26520cf274dab611fb6db42b60cf8d4cc2e334cc
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2024-11-01 12:53:02 -0400

    Add more routes

--- index.py
+++ index.py
@@ -4,5 +4,20 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index(name=None):
-    return render_template('index.html', person=name)
+def index():
+    return render_template('index.html', name='Index')
+
+
+@app.route("/login")
+def login():
+    return render_template('index.html', name='Login')
+
+
+@app.route("/home")
+def home():
+    return render_template('index.html', name='home')
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    return render_template('index.html', name=username)
